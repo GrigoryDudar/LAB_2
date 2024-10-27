692b6cd6b56318817e8485d714942f209ecd076e
GrigoryDudar <grishadudar@gmail.com>
2024-10-27 17:51:16 +0200

    LAB2

--- Task1/main.go
+++ Task1/main.go
@@ -1,63 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+import "fmt"
 
-func generatePassword(length int, small, large, numbers, symbols bool) string {
-	var letters = ""
-	if small {
-		letters += "abcdefghijklmnopqrstuvwxyz"
+func ReverseArray(arr []int) {
+	for i, j := 0, len(arr)-1; i < j; i, j = i+1, j-1 {
+		arr[i], arr[j] = arr[j], arr[i]
 	}
-	if large {
-		letters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	}
-	if numbers {
-		letters += "0123456789"
-	}
-	if symbols {
-		letters += "!@#$%^&*"
-	}
-
-	if letters == "" {
-		return "Помилка: виберіть хоча б один тип символів!"
-	}
-
-	password := make([]byte, length)
-	for i := range password {
-		password[i] = letters[rand.Intn(len(letters))]
-	}
-
-	return string(password)
 }
-
 func main() {
-	var length int
-	var small, large, numbers, symbols string
-
-	fmt.Print("Введіть довжину пароля: ")
-	fmt.Scan(&length)
-
-	fmt.Print("Чи використовувати малі літери? (+/-): ")
-	fmt.Scan(&small)
-
-	fmt.Print("Чи використовувати великі літери? (+/-): ")
-	fmt.Scan(&large)
-
-	fmt.Print("Чи використовувати цифри? (+/-): ")
-	fmt.Scan(&numbers)
-
-	fmt.Print("Чи використовувати спеціальні символи? (+/-): ")
-	fmt.Scan(&symbols)
-
-	password := generatePassword(
-		length,
-		small == "+",
-		large == "+",
-		numbers == "+",
-		symbols == "+",
-	)
-
-	fmt.Println("Згенерований пароль:", password)
+	arr := []int{1, 2, 3, 4, 5}
+	ReverseArray(arr)
+	fmt.Println("Reversed array:", arr)
 }
